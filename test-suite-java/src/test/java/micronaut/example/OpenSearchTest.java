56006a87023bfd5954b9a3dc260ce09d805ce563
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-06 16:45:29 +0100

    test pojo fields

--- test-suite-java/src/test/java/micronaut/example/OpenSearchTest.java
+++ test-suite-java/src/test/java/micronaut/example/OpenSearchTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class OpenSearchTest {
     @Test
     void testOpenSearch(MovieService movieService) {
+        String imdb = "KJFDOD";
         String title = "Die Hard";
         movieService.saveMovie(new Movie("KJFDOD", title));
         await().atMost(10, SECONDS).until(() ->
@@ -20,5 +22,7 @@ class OpenSearchTest {
         );
         Movie result = movieService.searchMovies(title);
         Assertions.assertNotNull(result);
+        assertEquals(title, result.getTitle());
+        assertEquals(imdb, result.getImdb());
     }
 }
