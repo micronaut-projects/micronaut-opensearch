1e2fbb07eaa36bb8a7195a5c42de8d48a5351561
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-20 10:41:07 +0100

    more test

--- opensearch/src/test/java/io/micronaut/opensearch/conf/OpenSearchConfigurationTest.java
+++ opensearch/src/test/java/io/micronaut/opensearch/conf/OpenSearchConfigurationTest.java
@@ -2,9 +2,11 @@ package io.micronaut.opensearch.conf;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.BeanContext;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Property(name = "micronaut.opensearch.default-headers[0]", value = "Foo:Bar")
 @MicronautTest(startApplication = false)
 class OpenSearchConfigurationTest {
 
@@ -29,6 +32,15 @@ class OpenSearchConfigurationTest {
         try (ApplicationContext ctx = ApplicationContext.run(Collections.singletonMap("micronaut.opensearch.enabled", StringUtils.FALSE))) {
             assertFalse(ctx.containsBean(OpenSearchConfiguration.class));
         }
+    }
+
+    @Test
+    void defaultHttpHeadersCanBeSetViaConfiguration() {
+        OpenSearchConfiguration openSearchConfiguration = beanContext.getBean(OpenSearchConfiguration.class);
+        assertEquals(1, openSearchConfiguration.getDefaultHeaders().length);
+        Header header = openSearchConfiguration.getDefaultHeaders()[0];
+        assertEquals("Foo", header.getName());
+        assertEquals("Bar", header.getValue());
     }
 
     @Test
