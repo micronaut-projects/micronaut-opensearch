6d2e9d4ee79c2b7f93312c64ec0e0a573cfd0875
Tim Yates <tim.yates@gmail.com>
2024-03-22 10:28:50 +0000

    Fix 21

--- test-suite-opensearch-container/build.gradle.kts
+++ test-suite-opensearch-container/build.gradle.kts
@@ -7,3 +7,11 @@ dependencies {
     api(libs.managed.opensearch.testcontainers)
     implementation(mnTestResources.micronaut.test.resources.server)
 }
+
+// We have to do this, as under Java 21 test-suite-kotlin will still use 17 (because of Kapt)
+// And if this is compiled by 21, we won't be able to use it...
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
