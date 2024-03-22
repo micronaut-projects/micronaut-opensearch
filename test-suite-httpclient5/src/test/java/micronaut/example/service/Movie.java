7ad8887b8cc8ab4fd77dfd0f6270530b11c7c481
Tim Yates <tim.yates@gmail.com>
2024-03-22 09:37:24 +0000

    Fix native httpclient5 test

--- test-suite-httpclient5/src/test/java/micronaut/example/service/Movie.java
+++ test-suite-httpclient5/src/test/java/micronaut/example/service/Movie.java
@@ -1,7 +1,9 @@
 package micronaut.example.service;
 
 import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.ReflectiveAccess;
 
 @Introspected
+@ReflectiveAccess
 public record Movie(String imdb, String title) {
 }
