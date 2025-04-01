1dc26b17ec98ee607dc4640d7fef2cac9f4628c2
Sergio del Amo <sergio.delamo@softamo.com>
2025-04-01 16:14:50 +0200

    annotate with @Reflective Access

--- test-suite-kotlin/src/test/kotlin/micronaut/example/service/Movie.kt
+++ test-suite-kotlin/src/test/kotlin/micronaut/example/service/Movie.kt
@@ -1,6 +1,8 @@
 package micronaut.example.service
 
 import io.micronaut.core.annotation.Introspected
+import io.micronaut.core.annotation.ReflectiveAccess
 
+@ReflectiveAccess
 @Introspected
 data class Movie(val imdb: String, val title: String)
