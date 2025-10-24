1381546b1cc49e50a5b94400d47194b11913f000
issam liqali <liqaliissam@gmail.com>
2025-10-24 10:07:56 +0100

    fix CVE(CVE-2025-8885)

--- opensearch-restclient/build.gradle.kts
+++ opensearch-restclient/build.gradle.kts
@@ -13,6 +13,12 @@ dependencies {
     runtimeOnly(mnLogging.slf4j.jcl.over.slf4j)
 
     testImplementation(mn.micronaut.jackson.databind)
+
+    constraints {
+        implementation("org.bouncycastle:bc-fips:2.0.1"){
+            because("Older versions have CVE(CVE-2025-8885) Vulnerability")
+        }
+    }
 }
 
 micronautBuild {
