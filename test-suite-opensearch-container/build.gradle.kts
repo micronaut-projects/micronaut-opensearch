d79b482c70c337f12aac1771c2aedd1f4a8319c0
issam liqali <liqaliissam@gmail.com>
2026-01-20 15:08:52 +0100

    update micronaut

--- test-suite-opensearch-container/build.gradle.kts
+++ test-suite-opensearch-container/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     api(libs.managed.opensearch.testcontainers)
     implementation(mnTestResources.micronaut.test.resources.server)
+    implementation(mn.micronaut.core.processor)
 }
 
 // We have to do this, as under Java 21 test-suite-kotlin will still use 17 (because of Kapt)
