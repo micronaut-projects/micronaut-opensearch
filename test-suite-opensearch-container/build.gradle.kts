3b00cd6e2d8759252c694a2b791a2a3a20caba1b
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-18 15:43:45 +0200

    sourceCompatibility 25

--- test-suite-opensearch-container/build.gradle.kts
+++ test-suite-opensearch-container/build.gradle.kts
@@ -10,9 +10,7 @@ dependencies {
     implementation(mn.micronaut.core.processor)
 }
 
-// We have to do this, as under Java 21 test-suite-kotlin will still use 17 (because of Kapt)
-// And if this is compiled by 21, we won't be able to use it...
 java {
-    sourceCompatibility = JavaVersion.toVersion("21")
-    targetCompatibility = JavaVersion.toVersion("21")
+    sourceCompatibility = JavaVersion.toVersion("25")
+    targetCompatibility = JavaVersion.toVersion("25")
 }
